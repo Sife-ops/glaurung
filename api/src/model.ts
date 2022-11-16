7860c57062ebab94ad162ace0753670a882bf924
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-16 14:17:57 -0800

    model

---
+++ api/src/model.ts
@@ -0,0 +1,34 @@
+import Sqlite3 from "better-sqlite3";
+import { Kysely, Generated, ColumnType, SqliteDialect } from "kysely";
+
+interface PersonTable {
+  id: Generated<number>;
+  first_name: string;
+  gender: "male" | "female" | "other";
+  last_name: string | null;
+  modified_at: ColumnType<Date, string | undefined, never>;
+}
+
+interface PetTable {
+  id: Generated<number>;
+  name: string;
+  owner_id: number;
+  species: "dog" | "cat";
+}
+
+interface MovieTable {
+  id: Generated<string>;
+  stars: number;
+}
+
+interface Database {
+  person: PersonTable;
+  pet: PetTable;
+  movie: MovieTable;
+}
+
+export const db = new Kysely<Database>({
+  dialect: new SqliteDialect({
+    database: new Sqlite3("db.sqlite"),
+  }),
+});
