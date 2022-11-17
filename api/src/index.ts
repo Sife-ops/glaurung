7189c53dae1ace113cc45cc4af0a46123d824c9b
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-16 19:49:13 -0800

    graphql

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,69 @@
+// import DataloaderPlugin from "@pothos/plugin-dataloader";
+import SchemaBuilder from "@pothos/core";
 import { db } from "./model";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+export const builder = new SchemaBuilder<{
+  Context: {
+    user: {
+      userId: string;
+      email: string;
+    };
+  };
+}>({
+  // plugins: [DataloaderPlugin],
+});
+
+builder.queryType({});
+builder.mutationType({});
+
+builder.queryFields((t) => ({
+  hello: t.string({
+    resolve: () => {
+      console.log("hello");
+      return "hello";
+    },
+  }),
+}));
+
+builder.mutationFields((t) => ({
+  mello: t.string({
+    resolve: () => "mello",
+  }),
+}));
+
+export const schema = builder.toSchema({});
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 const db_ = db(true);
 
+const app = express();
+const httpServer = http.createServer(app);
+
 (async () => {
-  console.log("hello");
+  const server = new ApolloServer({
+    schema,
+    // plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
 
-  const result = await db_
-    .insertInto("user")
-    .values({
-      username: "yeah",
-      password: "pass",
-    })
-    .executeTakeFirst();
+  app.use(cors(), bodyParser.json(), expressMiddleware(server));
 
-  console.log(result);
+  await new Promise(
+    (resolve) => httpServer.listen({ port: 4000 }, resolve as () => void) // todo: sus
+  );
+  console.log(`🚀 Server ready at http://localhost:4000`);
 })();
