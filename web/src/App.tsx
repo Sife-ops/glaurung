b1117f9d5e1bb795ec4b09efa3a6f76a35ee85ae
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-29 22:49:10 -0800

    graphiql

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
-// import reactLogo from "./assets/react.svg";
-// import { useEffect } from "react";
-// import { useServicesQuery } from "@glaurung/graphql/urql";
-// import { useState } from "react";
-import "./App.css";
+import "graphiql/graphiql.css";
+import React from "react";
+import { GraphiQL } from "graphiql";
 import { SignIn } from "./component/page/sign-in";
+import { createGraphiQLFetcher } from "@graphiql/toolkit";
 import { useAuthContext } from "./hook/auth-context";
+
 import {
   Outlet,
   BrowserRouter,
@@ -13,45 +13,28 @@ import {
   useNavigate,
   Navigate,
 } from "react-router-dom";
-import { Home } from "./component/page/home";
-import { CreateService } from "./component/page/create-service";
+
+const fetcher = createGraphiQLFetcher({
+  url: "http://localhost:4000",
+  headers: {
+    authorization: localStorage.getItem("accessToken") || "",
+  },
+});
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            // <UserContextProvider>
-            //   <Navigation />
-            //   <Spacer />
-            <PrivateRoutes />
-            // </UserContextProvider>
-          }
-        >
-          <Route path="/home" element={<Home />} />
-          <Route path="/create-service" element={<CreateService />} />
+        <Route path="/" element={<PrivateRoutes />}>
           <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<GraphiQL fetcher={fetcher} />} />
         </Route>
         <Route path="/sign-in" element={<SignIn />} />
-        {/* <Route path="/sign-in" element={<div>sign in</div>} /> */}
         <Route path="/error" element={<div>404</div>} />
         <Route path="*" element={<Navigate to="/error" />} />
-        {/* {import.meta.env.DEV && <Route path="/dev" element={<Dev />} />}
-        <Route path="/auth" element={<Auth to="/home" />} />
-        <Route path="/error" element={<div>404</div>} />
-        <Route path="*" element={<Navigate to="/error" />} /> */}
       </Routes>
-      {/* <Footer /> */}
     </BrowserRouter>
   );
-
-  // return (
-  //   <div>
-  //     <div>app</div>
-  //   </div>
-  // );
 }
 
 const PrivateRoutes: React.FC = () => {
@@ -59,8 +42,6 @@ const PrivateRoutes: React.FC = () => {
   const nav = useNavigate();
 
   if (!signedIn) {
-    // window.location.href =
-    //   import.meta.env.VITE_REGISTRAR_URL + "/sign-in?serviceId=feedshare";
     nav("/sign-in");
   }
 
